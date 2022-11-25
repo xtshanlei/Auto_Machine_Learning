ee9748aeeba67aa19453ebd611026824ae92bbd4
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-11-25 14:08:49 +0000

    Update AutomaticPredict.py

--- AutomaticPredict.py
+++ AutomaticPredict.py
@@ -10,3 +10,4 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
+    st.write(dataframe.columns)
