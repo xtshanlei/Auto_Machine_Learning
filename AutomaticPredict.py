ec6dca590634b77e0a44ac929d68b644120e816d
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-11-25 14:03:22 +0000

    Create AutomaticPredict.py

---
+++ AutomaticPredict.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from Auto_Pred import AutoPred
+import pandas as pd
+import numpy  as np
+import seaborn as sns
+
+#######Upload data files#######
+st.header('1. Upload dataset')
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
