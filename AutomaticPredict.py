8556f5dd4f4f84301679fba13aded4e4c0e34164
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-11-25 14:17:47 +0000

    Update AutomaticPredict.py

--- AutomaticPredict.py
+++ AutomaticPredict.py
@@ -10,10 +10,12 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
-    st.write(dataframe.columns)
     response = st.selectbox('Please select the response or dependent variable:',dataframe.columns)
-    outlier_columns=st.multiselect('Please select the columns that you want to remove outliers',dataframe.columns)
-    cat_columns =st.multiselect('Please select the categorical columns',dataframe.columns)
-    automl = AutoPred(dataframe,response,outlier_columns,cat_columns)
-    automl.remove_outlier_for_all_columns()
-    st.write(automl.clean_df)
+    if response:
+        cat_columns =st.multiselect('Please select the categorical columns',dataframe.columns)
+        if cat_columns:
+            outlier_columns=st.multiselect('Please select the columns that you want to remove outliers',dataframe.columns)
+            if outlier_columns:
+                automl = AutoPred(dataframe,response,outlier_columns,cat_columns)
+                automl.remove_outlier_for_all_columns()
+                st.write(automl.clean_df)
