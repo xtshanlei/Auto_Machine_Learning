57c27e496712a1fefb91fbe5799a833ff4951b00
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-11-25 15:27:43 +0000

    Update AutomaticPredict.py

--- AutomaticPredict.py
+++ AutomaticPredict.py
@@ -27,5 +27,5 @@ if uploaded_file is not None:
                     automl.train_test_data(train_size)
                     st.write('The dataset has been split into {} training and {} test samples'.format(len(automl.hf_train),len(automl.hf_test)))
                     st.header('5. Training')
-                    leader_model = automl.train(save_path)
+                    leader_model = automl.train()
                     st.download_button('Click to download the trained model', leader_model)
